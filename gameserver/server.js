16a2c73c164d2fa9c447fff7aa52f05b386cad38
root <root@ip-172-31-26-9.ap-northeast-2.compute.internal>
2019-02-20 10:38:19 +0000

    1902201938 openbeta phase final

--- gameserver/server.js
+++ gameserver/server.js
@@ -1,24 +1,27 @@
 const MATCHINTERVAL = 3000;
 const CLEANERINTERVAL = 1000;
 const PORT = 7376;
-
+const PRODUCTION = true;
 /*ssl server settings*/
-/*
+
 var fs = require("fs");
+
 var privatekey = fs.readFileSync('/etc/letsencrypt/live/omoku.net/privkey.pem').toString();
 var cert = fs.readFileSync('/etc/letsencrypt/live/omoku.net/cert.pem').toString();
 var ca = fs.readFileSync('/etc/letsencrypt/live/omoku.net/fullchain.pem').toString();
+
 var options = {
     key:privatekey,
     cert:cert,
     ca:ca
-}*/
+}
+
 var mysql = require("mysql");
 var db = mysql.createPool({
     connectionLimit:200,
     host:'localhost',
-    user:'root',
-    password:'wang0321!',
+    user:'omoku',
+    password:'wjdgml11!',
     database:'omoku'
 });
 var socketOption = {
@@ -26,10 +29,19 @@ var socketOption = {
     pingTimeout: 4000,
 }
 
-var http = require("http").Server(app).listen(PORT,function(){
-    console.log("listening on *:"+PORT);
-});
+var app = require("express")();
 
+if(PRODUCTION){
+var http = require("https").createServer(options,app).listen(PORT,function(){
+    console.log("HTTPS Server listening on *:"+PORT);
+});
+}else{
+var http = require("http").Server(app).listen(
+PORT,function(){
+console.log("HTTP Server listening on "+PORT);
+}
+);
+}
 var io = require("socket.io")(http,socketOption);
 var app,packetProcessor;
 var validateUserinfo = db.query("UPDATE users SET online = '0';",[],
@@ -39,7 +51,6 @@ function(error,results,fields){
         return;
     }
     console.log("DB서버에 연결 완료..");
-    app = require("express")();
     packetProcessor = require('./packetProcessor.js')(io,MATCHINTERVAL,db,CLEANERINTERVAL);
 });
 
