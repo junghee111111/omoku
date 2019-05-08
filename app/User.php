2260976b9fb6d8dee8984e680c5d5f5bb2765727
Wang Jeong Hee <jungheejjang1004@gmail.com>
2019-05-09 01:11:20 +0900

    회원가입 안되는 오류 수정

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name','wins','loses','gold','dol','board','online','remove','id'
+        'name','wins','loses','gold','dol','board','online','remove','id','email','password'
     ];
 
     /**
